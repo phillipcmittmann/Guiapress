90c3fb6b7cdd11851f90cbffd3eefda61e4759c3
Phillip <phillip.mittmann@gmail.com>
2021-01-01 14:55:51 -0300

    Alterando estrutura do projeto

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 
 const connection = require('./database/database');
 
-const categoriesController = require('./categories/CategoriesController');
-const articlesController = require('./articles/ArticlesController');
+const categoriesController = require('./controllers/CategoriesController');
+const articlesController = require('./controllers/ArticlesController');
 
 app.set('view engine', 'ejs');
 
