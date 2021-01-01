b2f7313399e690d10b6004c038051bdd25c805e8
Phillip <phillip.mittmann@gmail.com>
2021-01-01 14:53:57 -0300

    Alterando estrutura do projeto

---
+++ models/ArticlesModel.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const connection = require('../database/database');
+
+const ArticlesModel = connection.define('articles', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+module.exports = ArticlesModel;
