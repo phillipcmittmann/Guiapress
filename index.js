772df6a6bfb9c61c891f84b68a9452dc2e23bc5f
Phillip <phillip.mittmann@gmail.com>
2021-01-01 14:40:46 -0300

    Alterada estrutura para suportar controllers

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const connection = require('./database/database');
 
+const categoriesController = require('./controllers/categories/CategoriesController');
+const articlesController = require('./controllers/articles/ArticlesController');
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -20,6 +23,10 @@ connection
     .catch(err => {
         console.log(err)
     });
+
+app.use('/', categoriesController);
+
+app.use('/', articlesController);
 
 app.get('/', (req, res) => {
     res.render('index');
