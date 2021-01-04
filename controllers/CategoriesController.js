cb8556a79377da7038ea258c6db819945a12584a
Phillip <phillip.mittmann@gmail.com>
2021-01-04 08:25:33 -0300

    POST Categorias e Slugify

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const CategoriesModel = require('../models/CategoriesModel');
+
+const slugify = require('slugify');
+
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new')
+});
+
+router.post('/categories/save', (req, res) => {
+    var title = req.body.title;
+
+    if (title !== undefined) {
+        CategoriesModel.create({
+            title: title,
+            slug: slugify(title)
+        })
+        .then(() => {
+            res.redirect('/');
+        });
+    } else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;
