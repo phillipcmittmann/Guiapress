0e5a0dea43f797da9c7bded5e3d57649e3618267
Phillip <phillip.mittmann@gmail.com>
2021-01-04 09:35:38 -0300

    Excluindo categorias

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -33,4 +33,24 @@ router.get('/admin/categories', (req, res) => {
     });
 });
 
+router.post('/categories/delete', (req, res) => {
+    var id = req.body.id;
+
+    if (id !== undefined) {
+        if (!isNaN(id)) {
+            CategoriesModel
+            .destroy({
+                where: { id: id }
+            })
+            .then(() => {
+                res.redirect('/admin/categories');
+            });
+        }  else {
+            res.redirect('/admin/categories');
+        }
+    } else {
+        res.redirect('/admin/categories');
+    }
+});
+
 module.exports = router;
